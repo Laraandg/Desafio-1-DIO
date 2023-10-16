c6e7e2fd2487cb618ca6d8d4c1c58f4e44ae0775
Lara Andrade <lara.andradeg@gmail.com>
2023-10-16 15:47:51 -0300

    inital commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ContaTerminal {
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite a sua conta bancária!");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite também a sua agência");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite o seu saldo");
+        double saldo = scanner.nextDouble();
+
+        System.out.printf("Olá " + nome + " " + sobrenome + ", ");
+        System.out.printf("obrigado por criar uma conta em nosso banco, sua conta é " + conta + ", ");
+        System.out.println("agência " + agencia + " e seu saldo " + saldo + " já está disponível para saque.");
+
+
+    }
 }
